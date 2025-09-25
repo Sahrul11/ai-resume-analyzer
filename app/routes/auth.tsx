3d413bdf89cabcc59df7e8a1a12caeea6c0e01fe
Sahrul11 <sahrulchoudhari0@gmail.com>
2025-09-26 00:10:18 +0530

    Authenticate

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { usePuterStore } from "~/lib/puter";
+import {useLocation, useNavigate} from "react-router";
 
 export const meta = () => ([
     { title: 'CVision | Auth' },
@@ -7,6 +9,20 @@ export const meta = () => ([
 
 
 const Auth = () => {
+    const { isLoading, auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split('next=')[1];
+    const navigate = useNavigate();
+
+
+
+    useEffect(() => {
+        if(auth.isAuthenticated) navigate(next);
+    }, [auth.isAuthenticated, next])
+
+
+
+
     return (
         <main className="bg-[url('/images/bg-auth.svg')] bg-cover min-h-screen flex items-center justify-center">
             <div className="gradient-border shadow-lg">
@@ -14,6 +30,26 @@ const Auth = () => {
                     <div className="flex flex-col items-center gap-2 text-center">
                         <h1>Welcome</h1>
                         <h2>Log In to Continue Your Job Journey</h2>
+                    </div>
+
+                    <div>
+                        {isLoading ? (
+                            <button className="auth-button animate-pulse">
+                                <p>Signing you in...</p>
+                            </button>
+                        ) : (
+                            <>
+                                {auth.isAuthenticated ? (
+                                    <button className="auth-button" onClick={auth.signOut}>
+                                        <p>Log Out</p>
+                                    </button>
+                                ) : (
+                                    <button className="auth-button" onClick={auth.signIn}>
+                                        <p>Log In</p>
+                                    </button>
+                                )}
+                            </>
+                        )}
                     </div>
                 </section>
 
